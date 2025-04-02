4a3c238e9c9c26fbef36b3f69c2f318f0c39f419
Asher Mancinelli <ashermancinelli@gmail.com>
2025-04-01 19:50:30 -0700

    inlining

--- lib/Dialect/OcamlDialect.cpp
+++ lib/Dialect/OcamlDialect.cpp
@@ -1,3 +1,8 @@
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/FunctionImplementation.h"
+#include "mlir/Transforms/InliningUtils.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/DialectInterface.h"
 #include "ocamlc2/Dialect/OcamlDialect.h"
 #include "mlir/IR/Attributes.h"
 #include "mlir/IR/Builders.h"
@@ -33,6 +38,38 @@ OpFoldResult ConvertOp::fold(ConvertOp::FoldAdaptor adaptor) {
   return nullptr;
 }
 
+namespace mlir {
+struct OcamlInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+  Operation *materializeCallConversion(OpBuilder &builder, Value input,
+                                        Type resultType,
+                                        Location conversionLoc) const final {
+    return builder.create<ConvertOp>(conversionLoc, resultType, input);
+  }
+  bool isLegalToInline(Operation *call, Operation *callable,
+                       bool wouldBeCloned) const final {
+    return true;
+  }
+  bool isLegalToInline(Operation *, Region *, bool,
+                       IRMapping &) const final {
+    return true;
+  }
+  bool isLegalToInline(Region *dest, Region *src, bool wouldBeCloned,
+                       IRMapping &valueMapping) const final {
+    return true;
+  }
+  void handleTerminator(Operation *op,
+                        ValueRange valuesToRepl) const final {
+    auto returnOp = cast<func::ReturnOp>(op);
+    assert(returnOp.getNumOperands() == valuesToRepl.size());
+    for (const auto &it : llvm::enumerate(returnOp.getOperands()))
+      valuesToRepl[it.index()].replaceAllUsesWith(it.value());
+  }
+};
+
+} // namespace mlir
+
+
 void OcamlDialect::initialize() {
   addTypes<
 #define GET_TYPEDEF_LIST
@@ -42,4 +79,5 @@ void OcamlDialect::initialize() {
 #define GET_OP_LIST
 #include "ocamlc2/Dialect/OcamlOps.cpp.inc"
   >();
+  addInterfaces<OcamlInlinerInterface>();
 }
