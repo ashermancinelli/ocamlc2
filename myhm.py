a7f6dc0b0d5806fc6c71b77e2a9beeafc0a49435
Asher Mancinelli <ashermancinelli@gmail.com>
2025-04-10 07:07:21 -0700

    checkpoint

--- myhm.py
+++ myhm.py
@@ -4,6 +4,44 @@ https://web.archive.org/web/20050420002559/http://www.cs.berkeley.edu/~nikitab/c
 https://raw.githubusercontent.com/rob-smallshire/hindley-milner-python/refs/heads/master/inference.py
 """
 
+import functools
+
+DEBUG = True
+
+
+def log(*a, **kw):
+    pass
+
+
+if DEBUG:
+
+    def log(*a, **kw):
+        print(*a, **kw)
+
+
+def logwrap(func):
+    """
+    A decorator that logs the function name, arguments, and return value.
+    Only active when DEBUG is True.
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if not DEBUG:
+            return func(*args, **kwargs)
+
+        args_strs = [str(a) for a in args]
+        kwargs_repr = [f"{k}={v}" for k, v in kwargs.items()]
+        signature = ", ".join(args_strs + kwargs_repr)
+
+        print(f"-- {func.__name__}({signature})")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} -> {result}")
+
+        return result
+
+    return wrapper
+
 
 class AST:
     pass
@@ -12,6 +50,7 @@ class AST:
 class Identifier(AST):
     def __init__(self, name):
         self.name = name
+
     def __str__(self):
         return self.name
 
@@ -19,6 +58,7 @@ class Identifier(AST):
 class IntLit(AST):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return str(self.value)
 
@@ -26,6 +66,7 @@ class IntLit(AST):
 class BoolLit(AST):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return str(self.value)
 
@@ -33,15 +74,17 @@ class BoolLit(AST):
 class BinOp(AST):
     def __init__(self, op, lhs, rhs):
         self.op, self.lhs, self.rhs = op, lhs, rhs
+
     def __str__(self):
-        return f'{self.lhs} {self.op} {self.rhs}'
+        return f"{self.lhs} {self.op} {self.rhs}"
 
 
 class Let(AST):
     def __init__(self, name, value, body):
         self.name, self.value, self.body = name, value, body
+
     def __str__(self):
-        return f'let {self.name} = {self.value} in {self.body}'
+        return f"let {self.name} = {self.value} in {self.body}"
 
 
 class Lambda(AST):
@@ -84,6 +127,7 @@ class TypeVariable:
     def instantiated(self) -> bool:
         return self.instance is not None
 
+    @logwrap
     def assign(self, type_expr: TypeExpr) -> None:
         self.instance = type_expr
 
@@ -97,6 +141,9 @@ class TypeVariable:
             return str(self.instance)
         return self.name()
 
+    def __repr__(self):
+        return str(self)
+
 
 class TypeOperator:
     def __init__(self, name, *types: list[TypeExpr]):
@@ -108,9 +155,12 @@ class TypeOperator:
             case 0:
                 return self.name()
             case [l, r]:
-                return f'({l} {self.name} {r})'
+                return f"({l} {self.name} {r})"
             case _:
-                return f'{self.name} {' '.join(self.types)}'
+                return f"{self.name}{' '.join(self.types)}"
+
+    def __repr__(self):
+        return str(self)
 
     def __getitem__(self, index):
         return self.types[index]
@@ -118,61 +168,58 @@ class TypeOperator:
 
 class Function(TypeOperator):
     def __init__(self, from_type, to_type):
-        super().__init__('->', from_type, to_type)
+        super().__init__("->", from_type, to_type)
+
 
 IntType = TypeOperator("int")
 BoolType = TypeOperator("bool")
 
 
 class TypeCheck:
-    def __init__(self, environment):
-        self.environment: dict[str, TypeExpr] = environment
+    def __init__(self):
+        # self.environment: dict[str, TypeExpr] = environment
+        pass
 
-    def infer(self, ast, concrete_types: list[TypeExpr] = None) -> TypeVariable:
-        if concrete_types is None:
-            concrete_types = set()
+    def __str__(self):
+        return "TypeCheck()"
 
+    @logwrap
+    def infer(self, ast, env, concrete_types: list[TypeExpr]) -> TypeVariable:
         match ast:
             case Identifier(name=name):
-                return self.typeof(name, concrete_types)
+                if name.isnumeric():
+                    return self.infer(IntLit(int(name)), env, concrete_types)
+                return self.typeof(name, env, concrete_types)
             case IntLit():
                 return IntType
             case BoolLit():
                 return BoolType
             case Apply(fn=fn, arg=arg):
-                fun_type = self.infer(fn, concrete_types)
-                arg_type = self.infer(arg, concrete_types)
+                fun_type = self.infer(fn, env, concrete_types)
+                arg_type = self.infer(arg, env, concrete_types)
                 result_type = TypeVariable()
                 self.unify_type_expressions(Function(arg_type, result_type), fun_type)
                 return result_type
             case Lambda(param=param, body=body):
                 arg_type = TypeVariable()
-                env = self.environment.copy()
-                concrete = concrete_types.copy()
-                self.environment[param] = arg_type
-                result_type = self.infer(body, concrete)
-                self.environment = env
-                concrete_types = concrete
+                result_type = self.infer(body, env.copy() | {param: arg_type}, concrete_types.copy())
                 return Function(arg_type, result_type)
             case Let(name=name, value=value, body=body):
-                value_type = self.infer(value, concrete)
-                env = self.environment.copy()
-                concrete = concrete_types.copy()
-                self.environment[name] = value_type
-                body_type = self.infer(body, concrete)
-                self.environment = env
-                concrete_types = concrete
+                value_type = self.infer(value, env, concrete_types)
+                body_type = self.infer(body, env.copy() | {name: value_type}, concrete_types.copy())
                 return body_type
             case _:
                 assert False
 
-    def typeof(self, name: str, concrete_types: set[TypeExpr]) -> TypeVariable:
-        if name in self.environment:
+    @logwrap
+    def typeof(self, name: str, env, concrete_types: set[TypeExpr]) -> TypeVariable:
+        if val := env.get(name, None):
             return self.deep_copy_with_new_generic_typevars(
-                self.environment[name], concrete_types
+                val, concrete_types
             )
         raise RuntimeError(f"Undefined symbol: {name}")
 
+    @logwrap
     def deep_copy_with_new_generic_typevars(
         self, type_variable: TypeExpr, concrete_type_vars: list[TypeExpr]
     ) -> TypeExpr:
@@ -194,10 +241,12 @@ class TypeCheck:
 
         return recursive_copy(type_variable)
 
+    @logwrap
     def prune_type_expression(self, type_expression: TypeExpr) -> TypeExpr:
         type_expression.instance = self.prune(type_expression.instance)
         return type_expression.instance
 
+    @logwrap
     def prune(self, type_expression: TypeExpr) -> TypeExpr:
         """
                 The function Prune is used whenever a type expression has to be inspected: it will always
@@ -213,12 +262,15 @@ class TypeCheck:
             case TypeOperator():
                 return type_expression
 
+    @logwrap
     def is_concrete(self, expr: TypeExpr, concrete_type_exprs: list[TypeExpr]) -> bool:
         return self.is_sub_type_expression_of_any(expr, concrete_type_exprs)
 
+    @logwrap
     def is_generic(self, expr: TypeExpr, concrete_type_exprs: list[TypeExpr]) -> bool:
         return not self.is_sub_type_expression_of_any(expr, concrete_type_exprs)
 
+    @logwrap
     def is_sub_type_expression_of_any(
         self, maybe_subexpr: TypeExpr, expr_iterable: list[TypeExpr]
     ) -> bool:
@@ -227,6 +279,7 @@ class TypeCheck:
             for member_type in expr_iterable
         )
 
+    @logwrap
     def is_sub_type_expression_of(
         self, maybe_subexpr: TypeExpr, expr: TypeExpr
     ) -> bool:
@@ -237,26 +290,28 @@ class TypeCheck:
             case TypeOperator():
                 return self.is_sub_type_expression_of_any(maybe_subexpr, expr.types)
 
+    @logwrap
     def unify_type_expressions(self, expr1: TypeExpr, expr2: TypeExpr) -> None:
         expr1, expr2 = self.prune(expr1), self.prune(expr2)
         match expr1:
             case TypeVariable():
-                if self.is_sub_type_expression_of(expr1, expr2) and expr1 != expr2:
-                    raise RuntimeError(
-                        f"Recursive unification when trying to unify these types: {expr1} {expr2}"
-                    )
-                expr1.assign(expr2)
+                if expr1 != expr2:
+                    if self.is_sub_type_expression_of(expr1, expr2):
+                        raise RuntimeError(
+                            f"Recursive unification when trying to unify these types: {expr1} {expr2}"
+                        )
+                    expr1.assign(expr2)
 
-            case TypeOperator():
+            case TypeOperator(name=n1, types=ts1):
                 match expr2:
                     case TypeVariable():
                         self.unify_type_expressions(expr2, expr1)
-                    case TypeOperator():
-                        if expr1.name != expr2.name or len(expr1.types) != len(
-                            expr2.types
-                        ):
+                    case TypeOperator(name=n2, types=ts2):
+                        log(n1, ts1)
+                        log(n2, ts2)
+                        if n1 != n2 or len(ts1) != len(ts2):
                             raise RuntimeError(f"Type mismatch: {expr1} {expr2}")
-                        for subtype1, subtype2 in zip(expr1.types, expr2.types):
+                        for subtype1, subtype2 in zip(ts1, ts2):
                             self.unify_type_expressions(subtype1, subtype2)
             case _:
                 print(expr1, expr2)
@@ -268,13 +323,14 @@ def typevars(how_many: int):
 
 
 def test(base_env, ast: AST):
-    tc = TypeCheck(base_env)
+    tc = TypeCheck()
     print(ast)
     try:
-        result = tc.infer(ast)
+        result = tc.infer(ast, base_env, set())
         print(result)
     except Exception as e:
         print(e)
+
 
 if __name__ == "__main__":
     pair_type = TypeOperator("*", *typevars(2))
@@ -283,22 +339,32 @@ if __name__ == "__main__":
         "false": BoolType,
         "*": Function(IntType, Function(IntType, IntType)),
         "tuple2": Function(pair_type[0], Function(pair_type[1], pair_type)),
+        "pair": Function(pair_type[0], Function(pair_type[1], pair_type)),
     }
 
-    ast = Lambda(
-        "x",
-        Apply(
-            Apply(Identifier("tuple2"), Apply(Identifier("x"), IntLit(3))),
-            Apply(Identifier("x"), BoolLit(True)),
+    tests = [
+        Lambda(
+            "x",
+            Apply(
+                Apply(Identifier("tuple2"), Apply(Identifier("x"), IntLit(3))),
+                Apply(Identifier("x"), BoolLit(True)),
+            ),
         ),
-    )
-    test(environment, ast)
-
-    ast = Lambda(
-        "x",
-        Apply(
-            Apply(Identifier("tuple2"), Apply(Identifier("x"), IntLit(3))),
-            Apply(Identifier("x"), IntLit(5)),
+        Lambda(
+            "x",
+            Apply(
+                Apply(Identifier("tuple2"), Apply(Identifier("x"), IntLit(3))),
+                Apply(Identifier("x"), IntLit(5)),
+            ),
         ),
-    )
-    test(environment, ast)
+        # Should fail:
+        # fn x => (pair ((x 3) ((x true)))
+        Lambda(
+            "x",
+            Apply(
+                Apply(Identifier("pair"), Apply(Identifier("x"), Identifier("3"))),
+                Apply(Identifier("x"), Identifier("true")),
+            ),
+        ),
+    ]
+    test(environment, tests[-1])
