3536bad6ba3a310a04044e24843d6fa0ab511eff
Asher Mancinelli <ashermancinelli@gmail.com>
2025-04-10 07:51:48 -0700

    checkpoint

--- myhm.py
+++ myhm.py
@@ -6,7 +6,7 @@ https://raw.githubusercontent.com/rob-smallshire/hindley-milner-python/refs/head
 
 import functools
 
-DEBUG = True
+DEBUG = False
 
 
 def log(*a, **kw):
@@ -46,10 +46,17 @@ def logwrap(func):
 class AST:
     pass
 
+class Apply(AST): ...
 
 class Identifier(AST):
     def __init__(self, name):
         self.name = name
+
+    def __call__(self, *args: AST):
+        app = Apply(self, args[0])
+        for arg in args[1:]:
+            app = Apply(app, arg)
+        return app
 
     def __str__(self):
         return self.name
@@ -92,8 +99,7 @@ class Lambda(AST):
         self.param, self.body = param, body
 
     def __str__(self):
-        return f"(\\{self.param} -> {self.body})"
-
+        return f"(λ{self.param} . {self.body})"
 
 class Apply(AST):
     def __init__(self, fn, arg):
@@ -202,7 +208,7 @@ class TypeCheck:
                 return result_type
             case Lambda(param=param, body=body):
                 arg_type = TypeVariable()
-                result_type = self.infer(body, env.copy() | {param: arg_type}, concrete_types.copy())
+                result_type = self.infer(body, env.copy() | {param: arg_type}, concrete_types.copy() | {arg_type})
                 return Function(arg_type, result_type)
             case Let(name=name, value=value, body=body):
                 value_type = self.infer(value, env, concrete_types)
@@ -322,14 +328,19 @@ def typevars(how_many: int):
     return [TypeVariable() for _ in range(how_many)]
 
 
-def test(base_env, ast: AST):
-    tc = TypeCheck()
-    print(ast)
-    try:
-        result = tc.infer(ast, base_env, set())
-        print(result)
-    except Exception as e:
-        print(e)
+def test(base_env, ast: AST | list[AST]):
+    match ast:
+        case list():
+            for a in ast:
+                test(base_env, a)
+        case _:
+            tc = TypeCheck()
+            print(ast)
+            try:
+                result = tc.infer(ast, base_env, set())
+                print(result)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
@@ -342,29 +353,19 @@ if __name__ == "__main__":
         "pair": Function(pair_type[0], Function(pair_type[1], pair_type)),
     }
 
+    x = Identifier("x")
+    pair = Identifier("pair")
+    lit5 = IntLit(5)
+    lit3 = IntLit(3)
+    true = BoolLit(True)
+    false = BoolLit(False)
+
     tests = [
-        Lambda(
-            "x",
-            Apply(
-                Apply(Identifier("tuple2"), Apply(Identifier("x"), IntLit(3))),
-                Apply(Identifier("x"), BoolLit(True)),
-            ),
-        ),
-        Lambda(
-            "x",
-            Apply(
-                Apply(Identifier("tuple2"), Apply(Identifier("x"), IntLit(3))),
-                Apply(Identifier("x"), IntLit(5)),
-            ),
-        ),
         # Should fail:
         # fn x => (pair ((x 3) ((x true)))
-        Lambda(
-            "x",
-            Apply(
-                Apply(Identifier("pair"), Apply(Identifier("x"), Identifier("3"))),
-                Apply(Identifier("x"), Identifier("true")),
-            ),
-        ),
+        Lambda(x.name, pair(x(lit5), x(true))),
+        # Should pass
+        Lambda(x.name, pair(x(lit5), x(lit3))),
+        Lambda(x.name, pair(x(true), x(false))),
     ]
-    test(environment, tests[-1])
+    test(environment, tests)
