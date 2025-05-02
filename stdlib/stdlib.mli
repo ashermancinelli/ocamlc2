19f5ce55bfef8682f2393b53734fde8ef01a4432
Asher Mancinelli <ashermancinelli@gmail.com>
2025-05-02 14:30:17 -0700

    modules checkpoint (tests pass)

--- stdlib/stdlib.mli
+++ stdlib/stdlib.mli
@@ -5,14 +5,38 @@ type unit
 type bool
 type 'a list
 type 'a array
-type 'a option
+type 'a option = None | Some of 'a
 
 val sqrt : float -> float
 val print_int : int -> unit
 val print_endline : string -> unit
+val print_newline : unit -> unit
 val print_string : string -> unit
 val print_int : int -> unit
 val print_float : float -> unit
 val string_of_int : int -> string
 val float_of_int : int -> float
 val int_of_float : float -> int
+val ( + ) : int -> int -> int
+val ( - ) : int -> int -> int
+val ( * ) : int -> int -> int
+val ( / ) : int -> int -> int
+val ( % ) : int -> int -> int
+val ( < ) : int -> int -> bool
+val ( *. ) : float -> float -> float
+val ( /. ) : float -> float -> float
+val ( <. ) : float -> float -> bool
+val ( +. ) : float -> float -> float
+val ( -. ) : float -> float -> float
+val ( <= ) : int -> int -> bool
+val ( > ) : int -> int -> bool
+val ( >= ) : int -> int -> bool
+val ( = ) : int -> int -> bool
+val ( <> ) : int -> int -> bool
+val ( && ) : bool -> bool -> bool
+val ( || ) : bool -> bool -> bool
+val ( @@ ) : ('a -> 'b) -> 'a -> 'b
+val ( ^ ) : string -> string -> string
+val ( = ) : 'a -> 'a -> bool
+val ( <> ) : 'a -> 'a -> bool
+val ( < ) : 'a -> 'a -> bool
---
+++ stdlib/string.mli
@@ -0,0 +1,7 @@
+val string_of_int : int -> string
+val int_of_string : string -> int
+val float_of_string : string -> float
+val string_of_float : float -> string
+val float_of_int : int -> float
+val int_of_float : float -> int
+val concat : string -> string list -> string
